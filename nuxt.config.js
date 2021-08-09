f4498cb91b786a30e5e4834c2024c65651706017
satoru02 <sugiwaka02@gmail.com>
2021-08-09 20:15:04 +0900

    #2 add components

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'num',
+    title: '打ち込み写経 | 写経が出来るWebサービス',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -49,7 +49,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'ja'
     }
   },
 
---
+++ pages/mantra/_slug.vue
@@ -0,0 +1,7 @@
+<template>
+　<div>お経</div>
+</template>
+
+<script>
+export default {}
+</script>
