27fd6d43fa03eb2c4477787cfe876b3cfcd8dd76
satoru02 <sugiwaka02@gmail.com>
2021-08-22 23:40:09 +0900

    #10 add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '打ち込み写経 | 写経が出来るWebサービス',
+    title: '打ち込み写経 | 心を鎮める写経アプリ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
