9ec76452b8c64aa8c7e8a43cd562359961166022
satoru02 <sugiwaka02@gmail.com>
2021-08-30 12:55:17 +0900

    #15 add header menu

--- components/Header.vue
+++ components/Header.vue
@@ -5,12 +5,28 @@
         お経ちゃん
       </span>
     </div>
-    <div class="flex justify-end pr-6">
-      <BaseIcon icon-name="icon-menu-mark" :viewBox="'0 0 512 512'" :iconColor="'#111111'" :height="'25'"
-        :width="'25'">
-        <MenuIcon />
-      </BaseIcon>
-    </div>
+
+    <ul class="menu flex justify-end">
+      <li class="grid grid-cols-1"
+       @click="dropMenu = true"
+       @mouseover="dropMenu = true" @mouseleave="dropMenu = false">
+        <a class="flex justify-end">
+          <BaseIcon class="cursor-pointer" icon-name="icon-menu-mark" :viewBox="'0 0 512 512'" :iconColor="'#111111'" :height="'25'"
+            :width="'25'">
+            <MenuIcon />
+          </BaseIcon></a>
+        <transition name="fade" class="rounded">
+          <ul v-if="dropMenu" @click="dropMenu = false">
+            <li v-for="(menu, index) in sutras" :key="index">
+              <a @click="sutraPage(menu.name)" class="cursor-pointer">
+                {{ menu.name }}
+              </a>
+            </li>
+          </ul>
+        </transition>
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -24,10 +40,78 @@
       BaseIcon,
       MenuIcon,
     },
+    props: {
+      sutras: {
+        type: Array,
+      }
+    },
+    data() {
+      return {
+        dropMenu: false
+      }
+    },
     methods: {
-      movePath(){
+      movePath() {
         this.$router.replace(`/`)
+      },
+      sutraPage(sutra) {
+        this.$router.push(`/sutras/${sutra}`)
       }
     }
   }
+
 </script>
+
+<style lang="postcss">
+  .menu {
+    font: 14px/1.5 'Open Sans', sans-serif;
+    font-weight: 600;
+    margin-right: 24px;
+    padding: 0;
+    list-style: none;
+  }
+
+   .menu li ul li a {
+    display: block;
+    padding: 20px;
+    color: rgb(255, 255, 255);
+    text-decoration: none;
+  }
+
+  .menu li {
+    display: block;
+    float: left;
+    position: relative;
+    background: rgb(255, 255, 255);
+    color: #fff;
+    min-width: 180px;
+  }
+
+  .menu li ul {
+    position: absolute;
+    left: 0;
+    top: 25px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .menu li ul li {
+    background: rgb(0, 0, 0);
+    transition: background .2s;
+  }
+
+  .menu li ul li:hover {
+    background: rgb(185, 185, 185);
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .2s;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0;
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <Header />
+    <Header :sutras="sutras" />
     <SecondHeader />
 
     <div class="grid grid-cols-3">
